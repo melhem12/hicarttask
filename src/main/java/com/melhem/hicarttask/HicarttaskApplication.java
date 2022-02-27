6603e20090eaa50dd8e5b2677788f34cacea6cf2
Claims <ysfharb@hotmail.com>
2022-02-27 18:05:57 +0200

    nhhh

---
+++ src/main/java/com/melhem/hicarttask/HicarttaskApplication.java
@@ -0,0 +1,13 @@
+package com.melhem.hicarttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HicarttaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HicarttaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melhem/hicarttask/HicarttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melhem.hicarttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HicarttaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
